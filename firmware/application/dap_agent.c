216e4ce15ad3772e114567416cd644ad63b986d6
lyj <knightlyj@163.com>
2022-08-13 12:58:36 +0800

    usb no data

--- firmware/application/dap_agent.c
+++ firmware/application/dap_agent.c
@@ -5,20 +5,28 @@
 #include "DAP_config.h"
 #include "DAP.h"
 
-typedef struct 
+
+typedef struct
 {
     uint8_t cmdBuff[DAP_PACKET_SIZE];
 } DapCmd_t;
 
-typedef struct
+
+uint8_t rspTotalBuff[DAP_PACKET_SIZE * DAP_PACKET_COUNT];
+
+CL_QUEUE_DEF_INIT(cmdQueue, DAP_PACKET_COUNT * 2, DapCmd_t, static);
+
+CL_Result_t DapAgent_AddPacket(const uint8_t *buff)
 {
-    uint8_t rspBuff[DAP_PACKET_SIZE];
-} DapRsp_t;
+    const DapCmd_t* dapCmd = (const DapCmd_t*)buff;
+    CL_QueueAdd(&cmdQueue, dapCmd);
 
-
+    return CL_ResSuccess;
+}
 
 void DapAgent_Init(void)
 {
+
 }
 
 void DapAgent_Process(void)
@@ -26,7 +34,3 @@ void DapAgent_Process(void)
 
 }
 
-void DapAgent_Test(void)
-{
-
-}
